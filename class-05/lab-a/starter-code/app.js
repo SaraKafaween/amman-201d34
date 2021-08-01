ff96d156660242e1c361ded0c23cb70e0e625c61
sara kafaween <sarakafaween@yahoo.com>
2021-08-01 16:13:05 +0300

    multiplyarra

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -106,12 +106,14 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
-
+function multiplyyArra(multArr) { //eslint-disable-line
+  let x = multiply(multArr[0], multArr[1])[0];
+  let y = multiply(x, multArr[2])[0];
+  return[y, `the multiply of the array elements.`];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArra(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
